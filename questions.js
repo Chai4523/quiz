c6b513bf03b55cd3c9f9f0bc93a552e1b12fa01a
Chai Chung Yee <chungyee@fomatictech.com>
2024-12-30 13:12:23 +0800

    added 2 more questions to the list

--- questions.js
+++ questions.js
@@ -54,6 +54,42 @@ else {
         explaination:
           "NaN in Javascript is defined to be of type number despite its name(not a number).",
       },
+      {
+        question: "What will be the output of the following code snippet?",
+        snippet: `let s = "00000001111111";
+let l = 0, r = s.length - 1, ans = -1;
+while(l <= r) {
+   var mid = Math.floor((l + r) / 2);
+   if(s[mid] == '1') {
+       ans = mid;
+       r = mid - 1;
+   }
+   else {
+       l = mid + 1;
+   }
+}
+print(ans);`,
+        options: ["8", "7", "0", "1"],
+        answer: 1,
+        explaination:
+          "This code snippet shows one of the many applications of the binary search algorithm in Javascript. Here, we are binary searching for the index of the first occurrence of the character ‘1’ in the given string. When we get the character ‘1’ at the mid index, we store it as the answer and move to the left half which will have the first index of ‘1’ if it occurs. Else we move to the right half. So, the answer will be 7 (0-based indexing).",
+      },
+      {
+        question: "What will be the output of the following code snippet?",
+        snippet: `const example = ({ a, b, c }) => {
+ console.log(a, b, c);
+};
+example(0, 1, 2);`,
+        options: [
+          "0 1 2",
+          "0 Undefined Undefined",
+          "Undefined Undefined Undefined",
+          "None of the above",
+        ],
+        answer: 2,
+        explaination:
+          "Since we are passing individual numbers rather than a single object to the function, Javascript will initialize the object parameters with their default value of undefined.",
+      },
     ],
   },
 ];
